4bba7f83efab12f3e110374eaf2a9bad683682bd
z12witty <z12witty@gmail.com>
2024-02-19 03:34:33 +0900

    Todo

--- src/Page/RecyclePage.js
+++ src/Page/RecyclePage.js
@@ -1,33 +1,54 @@
-import { Component } from "react";
-import Title from '../Todolist/Title';
-import Content from "../Todolist/Content";
-import DonePage from "../Todolist/DonePage";
+import { useState } from 'react';
+import TodoCreate from '../components/TodoCreate';
+import TodoList from '../components/TodoList';
 
-export default class RecyclePage extends Component {
-    render() {
-        return (
-            <div style={{
-                display: "flex",
-                justifyContent: "space-between",
-                fontFamily: "Arial, sans-serif",
-                padding: "20px"
-            }}>
-                <div style={{ flex: 1, marginBottom: "20px" }}>
-                    <div style={{ marginBottom: "20px" }}>
-                        <h1 style={{ color: "#333", textAlign: "center" }}>Recycle Page</h1>
-                    </div>
-                    <div>
-                        <Title text="Todo List" />
-                        <Content />
-                    </div>
-                </div>
-                <div style={{ flex: 1, marginLeft: "40px" }}>
-                    <div style={{ marginBottom: "20px" }}>
-                        <h1 style={{ color: "#333", textAlign: "center", fontSize: "24px", marginTop: 0}}>Done</h1>
-                    </div>
-                    {/* <Content /> */}
-                </div>
-            </div>
-        );
+
+function RecyclePage() {
+  const [todos, setTodos] = useState([]);
+
+  const onSubmitHandler = (newTodo) => {
+    setTodos([...todos, newTodo]);
+  };
+
+  const onToggleHandler = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id
+          ? {
+              ...todo,
+              isDone: !todo.isDone,
+            }
+          : todo
+      )
+    );
+  };
+
+  const onDeleteHandler = (id) => {
+    if (window.confirm('해당 투두를 정말로 삭제하시겠습니까?')) {
+      setTodos(todos.filter((todo) => todo.id !== id));
     }
+  };
+
+  return (
+    <div className="todo-wrapper">
+      <section className="create-todo">
+        <h1 className="title"> TO-DO LIST</h1>
+        <TodoCreate onSubmitHandler={onSubmitHandler} />
+      </section>
+      <TodoList
+        todos={todos.filter((todo) => !todo.isDone)}
+        listTitle={'🔥 진행중 🔥'}
+        onToggle={onToggleHandler}
+        onDelete={onDeleteHandler}
+      />
+      <TodoList
+        todos={todos.filter((todo) => todo.isDone)}
+        listTitle={'🎉 완료 🎉'}
+        onToggle={onToggleHandler}
+        onDelete={onDeleteHandler}
+      />
+    </div>
+  );
 }
+
+export default RecyclePage;
